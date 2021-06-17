0ac651a97912b1b0e76aeb206c4a8300ff0a6805
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-06-17 09:21:41 +0800

    Update __init__.py

--- getAdvertisements/__init__.py
+++ getAdvertisements/__init__.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "mongodb://hsneighborlycosmosacc:ULd2iw7gRbXVyxZrAn9ZJg0GRFwNNIV5TjLmAck34qIxcVIo27fltiHHFqY6OahdHZFuIFBkU0cWhFHs2SZVbQ==@hsneighborlycosmosacc.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@hsneighborlycosmosacc@"  # TODO: Update with appropriate MongoDB connection information
+        url = "mongodb://hsneighborlycosmosacc:N497a6dPS06suPIhd8bZKCo74aZ9IhFu6eXSYApTDJMIYh0mrMl2WaquvHZarIr4Z6bhPuuKfo9HQCXx53Ch5g==@hsneighborlycosmosacc.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@hsneighborlycosmosacc@"  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
         database = client['hsneighborlycosmosdb']
         collection = database['advertisements']
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         result = collection.find({})
         result = dumps(result)
         print("result")
-        print(result)
+        
         return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
     except:
         print("could not connect to mongodb")
