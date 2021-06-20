9d71d4f634401803c865562c6e1c95dca88660ac
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-06-20 09:46:12 +0800

    Update __init__.py

--- getAdvertisements/__init__.py
+++ getAdvertisements/__init__.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "mongodb://hsneighborlycosmosacc:ABj6ZRRaAhiaN6TDp2228spkVD9e6Nr4alH0O9FpeLowPv7pjOvsV5KlDtYpy8ahFk7VPnQsORtWWJN7zyZrgQ==@hsneighborlycosmosacc.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@hsneighborlycosmosacc@"  # TODO: Update with appropriate MongoDB connection information
+        url = "mongodb://hsneighborlycosmosacc:AgBKmTohDzNr11eCm32b1q5kLOIEye8xDzNSQwYNRuItfjKXF02BmLj6V8Q38BdTI1hVSH5Tr92nr9qlpTRxbQ==@hsneighborlycosmosacc.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@hsneighborlycosmosacc@"  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
         database = client['hsneighborlycosmosdb']
         collection = database['advertisements']
